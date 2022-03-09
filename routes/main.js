1ff724ff8d30ae748b637bbaf56d3cce323a61d3
KilerBotz <99480935+KilerBotz@users.noreply.github.com>
2022-03-09 12:21:58 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,10 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/docs', (req, res) => {
-    res.sendFile(__path + '/views/docs.html')
+    res.sendFile(__path + '/views/index.html')
 })
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/views/docs.html')
 })
 router.get('/api/game', (req, res) => {
     res.sendFile(__path + '/views/game.html')
